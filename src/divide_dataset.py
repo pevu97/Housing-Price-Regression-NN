949fce5da8bc2b44bee3f9eb1ec5b944fc94e7dd
pevu97 <126363770+pevu97@users.noreply.github.com>
2025-04-23 15:27:23 +0200

    Update and rename data_clean.py to divide_dataset.py

---
+++ src/divide_dataset.py
@@ -0,0 +1,20 @@
+def data_clean():
+  dataset = raw_dataset.copy()
+  dataset.dropna(inplace=True)
+  index_to_drop = dataset[dataset['median_house_value'] == 500001]
+  dataset = dataset.drop(index=index_to_drop.index)
+  dataset_dummies = pd.get_dummies(dataset, drop_first=True)
+
+def data_division():
+  train_dataset = dataset_dummies.sample(frac=0.8, random_state=0)
+  test_dataset = dataset_dummies.drop(train_dataset.index)
+  train_labels = train_dataset.pop('median_house_value')
+  test_labels = test_dataset.pop('median_house_value')
+
+  from sklearn.preprocessing import StandardScaler
+
+  scaler = StandardScaler()
+  train_dataset_scaled = pd.DataFrame(data = scaler.fit_transform(train_dataset), columns=train_dataset.columns)
+  test_dataset_scaled = pd.DataFrame(data = scaler.transform(test_dataset), columns=test_dataset.columns)
+
+  
