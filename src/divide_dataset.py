c99231c1ccff96efdb5f089e5d6e3b49eb2aa7c2
pevu97 <126363770+pevu97@users.noreply.github.com>
2025-04-23 15:38:00 +0200

    Update divide_dataset.py

--- src/divide_dataset.py
+++ src/divide_dataset.py
@@ -3,7 +3,7 @@ def data_clean():
   dataset.dropna(inplace=True)
   index_to_drop = dataset[dataset['median_house_value'] == 500001]
   dataset = dataset.drop(index=index_to_drop.index)
-  dataset_dummies = pd.get_dummies(dataset, drop_first=True)
+  return dataset_dummies = pd.get_dummies(dataset, drop_first=True)
 
 def data_division():
   train_dataset = dataset_dummies.sample(frac=0.8, random_state=0)
@@ -16,5 +16,8 @@ def data_division():
   scaler = StandardScaler()
   train_dataset_scaled = pd.DataFrame(data = scaler.fit_transform(train_dataset), columns=train_dataset.columns)
   test_dataset_scaled = pd.DataFrame(data = scaler.transform(test_dataset), columns=test_dataset.columns)
+
+  return train_dataset_scaled, train_labels, test_dataset_scaled, test_labels
+  
 
   
