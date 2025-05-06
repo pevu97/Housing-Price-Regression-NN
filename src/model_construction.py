460434420176e07561c6bf8a3dbdc71cf18af33d
pevu97 <126363770+pevu97@users.noreply.github.com>
2025-05-06 14:12:20 +0200

    Update model_construction.py

--- src/model_construction.py
+++ src/model_construction.py
@@ -8,9 +8,6 @@ from tensorflow.keras.callbacks import EarlyStopping
 def build_model(input_dim):
 
    model = Sequential()
-   model.add(Dense(512, input_shape=[input_dim], activation='relu'))
-   model.add(Dropout(0.3)) 
-   
    model.add(Dense(256, activation='relu'))
    model.add(Dropout(0.3)) 
    
